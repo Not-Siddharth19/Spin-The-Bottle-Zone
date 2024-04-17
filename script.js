c2f605b2f58be8c90ec0689399bc120f0f63f4d1
Siddharth <siddharth87.mail@gmail.com>
2024-04-17 23:03:12 +0530

    nav bar

--- script.js
+++ script.js
@@ -308,3 +308,13 @@ function closeDarePopup() {
   darePopup.style.display = 'none';
   window.location.reload(); // Reload the page
 }
+
+//MENU Toggle
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
